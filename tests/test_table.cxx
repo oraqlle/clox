08c150ee138ed2aa5885a2ca40f202fbfc0e0367
oraqlle <41113853+oraqlle@users.noreply.github.com>
2025-11-07 14:43:34 +1100

    D2: Insertion and Lookup of table entries is now tested for

--- tests/test_table.cxx
+++ tests/test_table.cxx
@@ -1,4 +1,3 @@
-#include "table.h"
 #include <catch2/catch_test_macros.hpp>
 
 // Link clox library objects as C and not as C++
@@ -6,6 +5,10 @@ extern "C" {
 #include <string.h>
 
 #include "common.h"
+#include "object.h"
+#include "natives.h"
+#include "table.h"
+#include "value.h"
 #include "vm.h"
 
 // Copy-pasted hash function from object.c
@@ -24,6 +27,7 @@ static uint32_t hashStr(const char *key, size_t length) {
 }
 
 TEST_CASE("Finding Interned Strings", "[table][lookup]") {
+
     VM vm;
     initVM(&vm);
 
@@ -70,6 +74,316 @@ TEST_CASE("Finding Interned Strings", "[table][lookup]") {
     }
 }
 
-TEST_CASE("Set Table Entries", "[table][entry]") {}
+TEST_CASE("Finding Table Entries", "[table][lookup]") {
 
-TEST_CASE("Finding Table Entries", "[table][lookup]") {}
+    VM vm;
+    initVM(&vm);
+
+    SECTION("Lookup in VM::globals table") {
+
+        SECTION("Empty string key") {
+
+            const char *str = "";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ len,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ hash,
+            };
+
+            REQUIRE_FALSE(tableGet(&vm.globals, &key, &val));
+            REQUIRE(IS_NIL(val));
+        }
+
+        SECTION("Existing string key") {
+
+            const char *str = "clock";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString *key = tableFindString(&vm.strings, str, len, hash);
+
+            REQUIRE(tableGet(&vm.globals, key, &val));
+            REQUIRE(IS_NATIVE(val));
+            
+            ObjNative *ntv = ((ObjNative *)AS_OBJ(val));
+            REQUIRE(ntv->arity == 0);
+            REQUIRE(ntv->func == clockNative);
+        }
+
+        SECTION("Non-existing string key") {
+
+            const char *str = "format";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ len,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ hash,
+            };
+
+            REQUIRE_FALSE(tableGet(&vm.globals, &key, &val));
+            REQUIRE(IS_NIL(val));
+        }
+
+        SECTION("Invalid string key") {
+
+            const char *str = NULL;
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ 0,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ 0,
+            };
+
+            REQUIRE_FALSE(tableGet(&vm.globals, &key, &val));
+            REQUIRE(IS_NIL(val));
+        }
+    }
+
+    SECTION("Lookup in VM::strings table") {
+
+        SECTION("Empty string key") {
+
+            const char *str = "";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ len,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ hash,
+            };
+
+            REQUIRE_FALSE(tableGet(&vm.strings, &key, &val));
+            REQUIRE(IS_NIL(val));
+        }
+
+        SECTION("Existing string key") {
+
+            const char *str = "clock";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString *key = tableFindString(&vm.strings, str, len, hash);
+
+            REQUIRE(tableGet(&vm.globals, key, &val));
+            REQUIRE(IS_STRING(val));
+            
+            ObjString *obj = ((ObjString *)AS_OBJ(val));
+            REQUIRE(key == obj);
+            REQUIRE(key->length == len);
+            REQUIRE(key->hash == hash);
+            REQUIRE(strcmp(key->chars, str) == 0);
+        }
+
+        SECTION("Non-existing string key") {
+
+            const char *str = "format";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ len,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ hash,
+            };
+
+            REQUIRE_FALSE(tableGet(&vm.strings, &key, &val));
+            REQUIRE(IS_NIL(val));
+        }
+
+        SECTION("Invalid string key") {
+
+            const char *str = NULL;
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ 0,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ 0,
+            };
+
+            REQUIRE_FALSE(tableGet(&vm.strings, &key, &val));
+            REQUIRE(IS_NIL(val));
+        }
+    }
+}
+
+TEST_CASE("Set Table Entries", "[table][entry]") {
+
+    VM vm;
+    initVM(&vm);
+
+    SECTION("Setting VM::globals table") {
+
+        SECTION("Empty string key") {
+
+            const char *str = "";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ len,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ hash,
+            };
+
+            REQUIRE_FALSE(tableSet(&vm, NULL, &vm.globals, &key, val));
+        }
+
+        SECTION("Existing string") {
+
+            const char *str = "clock";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+            ObjString *key = tableFindString(&vm.strings, str, len, hash);
+
+            REQUIRE_FALSE(tableSet(&vm, NULL, &vm.globals, key, val));
+        }
+
+        SECTION("Non-existing string key") {
+
+            const char *str = "format";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ len,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ hash,
+            };
+
+            REQUIRE(tableSet(&vm, NULL, &vm.globals, &key, val));
+        }
+
+        SECTION("Invalid string key") {
+
+            const char *str = NULL;
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ 0,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ 0,
+            };
+
+            REQUIRE_FALSE(tableSet(&vm, NULL, &vm.globals, &key, val));
+        }
+    }
+
+    SECTION("Setting VM::strings table") {
+
+        SECTION("Empty string key") {
+
+            const char *str = "";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ len,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ hash,
+            };
+
+            REQUIRE_FALSE(tableSet(&vm, NULL, &vm.strings, &key, val));
+        }
+
+        SECTION("Existing string") {
+
+            // Possibly a malformed test case due to needing
+            // the pointer of the exact string object to
+            // successfully compare keys due to string interning
+            // SKIP()
+
+            const char *str = "clock";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+            ObjString *key = tableFindString(&vm.strings, str, len, hash);
+
+            REQUIRE_FALSE(tableSet(&vm, NULL, &vm.strings, key, val));
+        }
+
+        SECTION("Non-existing string key") {
+
+            const char *str = "format";
+            size_t len = strlen(str);
+            uint32_t hash = hashStr(str, len);
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ len,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ hash,
+            };
+
+            REQUIRE(tableSet(&vm, NULL, &vm.strings, &key, val));
+        }
+
+        SECTION("Invalid string key") {
+
+            const char *str = NULL;
+            Value val = NIL_VAL;
+
+            ObjString key = {
+                /* obj */ {/* type */ OBJ_STRING,
+                           /* isMarked */ false,
+                           /* next */ NULL},
+                /* length */ 0,
+                /* chars */ const_cast<char *>(str),
+                /* hash */ 0,
+            };
+
+            REQUIRE_FALSE(tableSet(&vm, NULL, &vm.strings, &key, val));
+        }
+    }
+}
+
